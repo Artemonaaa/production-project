1a0e182688d127e6c2f56b7c4713466d66e7b49b
artemonaaa <artemidaabc@gmail.com>
2025-03-06 17:58:21 +0200

    Add classNames for multiple classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Suspense,  } from "react";
+import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { useTheme } from "./theme/useTheme";
 
 import "./styles/index.scss";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Change Them</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
