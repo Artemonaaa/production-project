7e99c13e2ec62a286434f1a46535f53d90eea8f3
artemonaaa <artemidaabc@gmail.com>
2025-03-17 18:20:36 +0200

    Add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
@@ -7,17 +9,35 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import "./styles/index.scss";
 
+function MyComponent() {
+  const { t, i18n } = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === "ua" ? "en" : "ua")
+  };
+
+  return (
+    <>
+      <button onClick={toggle}>{t("translate")}</button>
+      <h1>{t("Welcome to React")}</h1>
+    </>
+  );
+}
+
 const App = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
+    <Suspense fallback="">
+      <div className={classNames("app", {}, [theme])}>
+        <Navbar />
+        <MyComponent />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
