3a6c672523c32995c8bfb242277edcc6374bb4f6
artemonaaa <artemidaabc@gmail.com>
2025-04-30 11:51:40 +0300

    Add counter with Redux-toolkit

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,8 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
+    'no-param-reassign': 'off',
+    'quotes': 'off'
   },
   globals: {
     __IS_DEV__: true,
