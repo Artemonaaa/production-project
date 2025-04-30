a768faef709c0e32a1b31c246075502545d8da70
artemonaaa <artemidaabc@gmail.com>
2025-04-30 11:52:20 +0300

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'no-param-reassign': 'off',
-    'quotes': 'off'
+    quotes: 'off',
   },
   globals: {
     __IS_DEV__: true,
