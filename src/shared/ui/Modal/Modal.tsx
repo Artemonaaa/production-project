d05c585076ec15eeaa3cac23c227998e56549aa7
artemonaaa <artemidaabc@gmail.com>
2025-05-20 13:20:56 +0300

    Improve theme and modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -59,7 +59,7 @@ export const Modal = (props: ModalProps) => {
 
   return (
     <Portal>
-      <div className={classNames(cls.modal, mods, [className, cls[theme]])}>
+      <div className={classNames(cls.modal, mods, [className, theme, 'app_modal'])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>{children}</div>
         </div>
